6134ad18f50e55d1c441a7f256ede24742e01b2f
SANDRO PEGORARO MANZANEDO <sandropegman@monlau.com>
2026-03-19 18:23:48 +0100

    Actua

--- aplicacion-facial/microservicio_facial/main.py
+++ aplicacion-facial/microservicio_facial/main.py
@@ -11,19 +11,19 @@ async def verify_img(img1: UploadFile = File(...), img2: UploadFile = File(...))
     path2 = f"temp_{img2.filename}"
 
     try:
-        # PASO A: Guardar imágenes temporales
+
         with open(path1, "wb") as buffer:
             shutil.copyfileobj(img1.file, buffer)
         with open(path2, "wb") as buffer:
             shutil.copyfileobj(img2.file, buffer)
 
-        # PASO B: Completar llamada a la IA
+
         result = DeepFace.verify(
             img1_path=path1,
             img2_path=path2,
             model_name="Facenet",
             detector_backend="opencv",
-            enforce_detection=False # Seguridad estricta activada
+            enforce_detection=False 
         )
 
         return {
@@ -32,6 +32,5 @@ async def verify_img(img1: UploadFile = File(...), img2: UploadFile = File(...))
             "model": result["model"]
         }
     finally:
-        # Limpieza de archivos temporales
         if os.path.exists(path1): os.remove(path1)
         if os.path.exists(path2): os.remove(path2)
